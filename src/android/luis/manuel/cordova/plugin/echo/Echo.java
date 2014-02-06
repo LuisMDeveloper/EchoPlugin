4f93d86c7535303852dce8a7ba85ad8f02d4214a
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2014-02-05 22:10:11 -0600

    initial commit

---
+++ src/android/luis/manuel/cordova/plugin/echo/Echo.java
@@ -0,0 +1,40 @@
+package luis.manuel.cordova.plugin.echo;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * Created by icarus on 2/5/14.
+ */
+public class Echo extends CordovaPlugin {
+
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action            The action to execute.
+     * @param args              JSONArry of arguments for the plugin.
+     * @param callbackContext   The callback id used when calling back into JavaScript.
+     * @return                  True if the action was valid, false if not.
+     */
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("echo")) {
+            String message = args.getString(0);
+            this.echo(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void echo(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+
+}
